7892e7f7cc42208418db13aabe2705d43121d1f4
xorgzz <118397053+xorgzz@users.noreply.github.com>
2023-11-15 20:25:46 +0100

    Update main.go

--- main.go
+++ main.go
@@ -37,11 +37,11 @@ func main () {
             os.Mkdir(finalLoc, 0755)
             srcPath := finalLoc + "/src"
             os.Mkdir(srcPath, 0755)
-            os.Mkdir(srcPath+"/"+pack, 755)
+            os.Mkdir(srcPath+"/"+pack, 0755)
             outPath := finalLoc + "/out"
             os.Mkdir(outPath, 0755)
             os.WriteFile(srcPath+"/"+pack+"/Main.java", []byte("package "+pack+";\n\npublic class Main {\n\tpublic static void main(String[] args) {\n\n\t}\n}"), 0666)
-            os.WriteFile(finalLoc+"/build.sh", []byte("#!/usr/bin/sh\n\njavac -d "+outPath+" "+srcPath+"/"+pack+"/*.java && cd out/"+pack+" && java Main"), 0766)
+            os.WriteFile(finalLoc+"/build.sh", []byte("#!/usr/bin/sh\n\njavac -d "+outPath+" "+srcPath+"/"+pack+"/*.java && cd out && java "+pack+".Main"), 0766)
         } else {
             fmt.Println("[!] Directory doesn't exist")
         }
