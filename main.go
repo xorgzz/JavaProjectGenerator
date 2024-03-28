980c7522238a0085d3f25b698d34141002c3e843
xorgzz <118397053+xorgzz@users.noreply.github.com>
2024-03-28 15:57:15 +0100

    Update main.go
    Added build and run file creation for Windows machines. Making it  both Linux and Windows friendly

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
-
 import (
 	"fmt"
 	"os"
 	"strings"
 )
 
-func exists(path string) bool {
+func exists(path string) (bool) {
 	info, err := os.Stat(path)
 	if !os.IsNotExist(err) && info.IsDir() {
 		return true
@@ -14,46 +13,40 @@ func exists(path string) bool {
 	return false
 }
 
-func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("Java Project Creator")
-		projLoc, err := os.Getwd()
-		var finalLoc string
-		var pack string
+func main () {
+	fmt.Println("Java Project Creator")
+	projLoc, err := os.Getwd()
+	var finalLoc string
+	var pack string
 
-		if err == nil {
-			fmt.Printf("Project directory: ")
-			fmt.Scan(&finalLoc)
-			if finalLoc[0] != '/' {
-				finalLoc = projLoc + "/" + finalLoc
-			}
-
-			fmt.Print("Project Alias: ")
-			fmt.Scan(&pack)
-
-			if pack == "" {
-				pack = "default"
-			}
-
-			tmp := strings.SplitAfter(finalLoc, "/")
-			path := strings.Join(tmp[0:len(tmp)-1], "")
-			if exists(path) {
-				fmt.Println("[!] Creating Project")
-				os.Mkdir(finalLoc, 0755)
-				srcPath := finalLoc + "/src"
-				os.Mkdir(srcPath, 0755)
-				os.Mkdir(srcPath+"/"+pack, 0755)
-				outPath := finalLoc + "/out"
-				os.Mkdir(outPath, 0755)
-				os.WriteFile(srcPath+"/"+pack+"/Main.java", []byte("package "+pack+";\n\npublic class Main {\n\tpublic static void main(String[] args) {\n\n\t}\n}"), 0666)
-				os.WriteFile(finalLoc+"/build.sh", []byte("#!/usr/bin/sh\nclear\n\njavac -d "+outPath+" "+srcPath+"/"+pack+"/*.java && cd "+outPath+" && java "+pack+".Main"), 0766)
-			} else {
-				fmt.Println("[!] Directory doesn't exist")
-			}
+	if err == nil {
+		fmt.Printf("Project directory: ")
+		fmt.Scan(&finalLoc)
+		if finalLoc[0] != '/' {
+		   finalLoc = projLoc + "/" + finalLoc 
+		}
+	
+		fmt.Print("Project Alias: ")
+		fmt.Scan(&pack)
+		
+		tmp := strings.SplitAfter(finalLoc, "/")
+		path := strings.Join(tmp[0:len(tmp)-1], "")
+		if exists(path) {
+			fmt.Println("[!] Creating Project")
+			os.Mkdir(finalLoc, 0755)
+			srcPath := finalLoc + "/src"
+			os.Mkdir(srcPath, 0755)
+			os.Mkdir(srcPath+"/"+pack, 0755)
+			outPath := finalLoc + "/out"
+			os.Mkdir(outPath, 0755)
+			os.WriteFile(srcPath+"/"+pack+"/Main.java", []byte("package "+pack+";\n\npublic class Main {\n\tpublic static void main(String[] args) {\n\n\t}\n}"), 0666)
+			os.WriteFile(finalLoc+"/build.sh", []byte("#!/usr/bin/sh\nclear\n\njavac -d "+outPath+" "+srcPath+"/"+pack+"/*.java && cd "+outPath+" && java "+pack+".Main"), 0766)
+			os.WriteFile(finalLoc+"/build.bat", []byte("@echo off\ncls\necho Running Java Project\njavac -d ./out src/"+pack+"/*.java && cd out/. && java "+pack+".Main\npause"), 0766)
 		} else {
-			fmt.Println(err)
+			fmt.Println("[!] Directory doesn't exist")
 		}
 	} else {
-		fmt.Println("[!] Too many args")
+		fmt.Println(err)
 	}
+	
 }
